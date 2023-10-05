d492666c6b38daf89770cebef1dd8ad325cde8bd
EsmeraldaEdith <esmeraslda.chulix@gmail.com>
2023-10-05 12:24:21 -0300

    terminado

--- 10_insert_teachers_subjects.sql
+++ 10_insert_teachers_subjects.sql
@@ -1,7 +1,7 @@
 \c new_school
 DELETE FROM teachers_subjects;
-INSERT INTO  teachers_subjects(subject_id, teachers_id)
-VALUES (1.1),
+INSERT INTO teachers_subjects(subject_id, teacher_id)
+VALUES(1,1),
     (2,2),
     (3,1),
     (4,2),
--- 11_inser students.sql
+++
@@ -1,8 +0,0 @@
-\c new_school
-DELETE FROM students;
-INSERT INTO students(name,lastaname)
-VALUES ('Esmeralda','Peñailillo'),
-       ('Coni','Reyes'),
-       ('jazmin','Valladares'),
-       ('Beleb','Keler'),
-       ('katie','Gonzales');
